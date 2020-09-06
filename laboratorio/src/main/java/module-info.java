3fc2188f0cb4442a61320754a3ef7962b31eb542
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-09-05 20:27:04 -0600

    crearcion de App maven

---
+++ laboratorio/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.una.laboratorio {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+
+    opens org.una.laboratorio to javafx.fxml;
+    exports org.una.laboratorio;
+}
---
+++ laboratorio/src/main/java/org/una/laboratorio/SecondaryController.java
@@ -0,0 +1,12 @@
+package org.una.laboratorio;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("login2");
+    }
+}
