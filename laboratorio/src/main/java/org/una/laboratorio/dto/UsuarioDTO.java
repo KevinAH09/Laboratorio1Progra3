bb500f7c06208c90edf8b860a67a9a34e7eb4cd6
Bosco <bosco11jico100@gmail.com>
2020-09-12 19:13:59 -0600

    AddEditWatchVariacion

--- laboratorio/src/main/java/org/una/laboratorio/dto/UsuarioDTO.java
+++ laboratorio/src/main/java/org/una/laboratorio/dto/UsuarioDTO.java
@@ -6,10 +6,6 @@
 package org.una.laboratorio.dto;
 
 import java.util.Date;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.una.laboratorio.dto.DepartamentoDTO;
 
 /**
